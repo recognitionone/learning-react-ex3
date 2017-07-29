d739a5f2e07b4f5bb9e43f9006b8d280dfad2311
Basia <Basia@MBP-Basia.home>
2017-07-29 13:59:41 +0200

    index.html and app.js ready for all evil

---
+++ app.js
@@ -0,0 +1,11 @@
+class MyAppComponent extends React.Component {
+	render() {
+		return(
+				<h1>Hello my evil minions</h1>
+			);
+	}
+}
+
+ReactDOM.render(
+	<MyAppComponent />,
+	document.getElementById("rootOfAllEvil"));
